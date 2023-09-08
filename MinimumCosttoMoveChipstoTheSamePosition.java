503c24e738980ad44fd595e9f524b59b6315034f
Iampra528 <iamprashant52870@gmail.com>
2023-09-09 00:09:47 +0530

    07/09/2023

---
+++ MinimumCosttoMoveChipstoTheSamePosition.java
@@ -0,0 +1,18 @@
+public class MinimumCosttoMoveChipstoTheSamePosition {
+    public static void main(String[] args) {
+        int[] arr = {2,3,3,3,3,3};
+        System.out.println(minCostToMoveChips(arr));
+    }
+
+    public static int minCostToMoveChips(int[] position) {
+        int even = 0 , odd = 0; 
+        for(int i = 0 ; i<position.length ; i++){
+            if(position[i]%2==0)
+                even++;
+            else
+                odd++;
+            
+        }
+        return Math.min(even,odd);
+    }
+}
